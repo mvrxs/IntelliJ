d0951689b3d1d203114bab2a3c5d0030a3e69db3
mvrxs <mdazsm@gmail.com>
2022-12-16 09:49:11 +0100

    Commit aceptoelreto

---
+++ src/main/java/uf2practicas/Aceptoelreto.java
@@ -0,0 +1,19 @@
+package uf2practicas;
+
+import java.util.Scanner;
+
+public class Aceptoelreto {
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int number;
+        number = sc.nextInt();
+
+        if ((number>0) && (number<=5))
+        for(int i=0;i<number;i++)
+        {
+            System.out.println("Hola mundo.");
+
+    }
+}
+}
